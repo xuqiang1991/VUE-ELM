d1ac48d2ce86c7051d58aaf10cd047e5545fea05
amonk <xz95@outlook.com>
2017-05-09 11:32:17 +0800

    commit

---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    我是评论
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
---
+++ src/components/seller/seller.vue
@@ -0,0 +1,11 @@
+<template>
+  <div >
+    我是卖家
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
