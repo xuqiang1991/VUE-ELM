012d6556572a57b6c7138761205b9de0b445efe3
ldlood <xz95@outlook.com>
2017-05-17 23:10:14 +0800

    购物车实现

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -23,9 +23,9 @@
     <div class="shopcart-list" v-show="listShow" transition="fold">
       <div class="list-header">
         <div class="title">购物车</div>
-        <span class="empty">清空</span>
+        <span class="empty" @click="empty">清空</span>
       </div>
-      <div class="list-content">
+      <div class="list-content" v-el:list-content>
         <ul>
           <li class="food" v-for="food in selectFoods">
             <span class="name">{{food.name}}</span>
@@ -44,6 +44,7 @@
 
 <script type="text/ecmascript-6">
   import cartcontrol from 'components/cartcontrol/cartcontrol';
+  import BScroll from 'better-scroll';
 
   export default {
     props: {
@@ -123,6 +124,17 @@
             return false;
           }
           let show = !this.fold;
+          if (show) {
+            this.$nextTick(() => {
+                if (!this.scroll) {
+                  this.scroll = new BScroll(this.$els.listContent, {
+                    click: true
+                  });
+                } else {
+                  this.scroll.refresh();
+                }
+            });
+          }
           return show;
       }
     },
@@ -143,6 +155,12 @@
           return;
         }
         this.fold = !this.fold;
+      },
+      empty() {
+        console.log('aa');
+        this.selectFoods.forEach((food) => {
+          food.count = 0;
+        });
       }
     },
     transitions: {
@@ -191,6 +209,8 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
   .shopcart
     position: fixed
     left:0
@@ -305,4 +325,44 @@
         transform :translate3d(0,-100%,0)
       &.fold-enter,&.fold-leave
         transform :translate3d(0,0,0)
+      .list-header
+        height: 40px
+        line-height :40px
+        padding :0 18px
+        background :#f3f5f7
+        border-bottom :1px solid rgba(7,17,27,0.1)
+        .title
+          float :left;
+          font-size :14px
+          color :rgb(7,17,27)
+        .empty
+          float :right;
+          font-size :12px
+          color :rgb(0,160,220)
+      .list-content
+        padding :0 18px
+        max-height :217px
+        overflow :hidden
+        background :#fff
+        .food
+          position :relative
+          padding :12px 0
+          box-sizing :border-box
+          border-1px(rgba(7,17,27,0.1))
+          .name
+            line-height :24px
+            font-size :14px
+            color :rgb(7,17,27)
+          .price
+            position :absolute
+            right :90px
+            bottom :12px
+            line-height :24px
+            font-size :14px
+            font-weight :700
+            color :rgb(240,20,20)
+          .cartControl-wrapper
+            position :absolute
+            right :0
+            bottom :6px
 </style>
