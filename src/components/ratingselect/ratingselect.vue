548792e4a2e2483ab0faef05eedb5ac2c72a9da5
amonk <xz95@outlook.com>
2017-06-07 13:32:32 +0800

    commit

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="ratingselect">
     <div class="rating-type border-1px">
-      <span>{{desc.all}}</span>
-      <span>{{desc.positive}}</span>
-      <span>{{desc.negative}}</span>
+      <span @click="select(2,$event)" class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span class="count">47</span></span>
+      <span @click="select(0,$event)" class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">40</span></span>
+      <span @click="select(1,$event)" class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count">7</span></span>
     </div>
-    <div class="switch">
+    <div class="switch" :class="{'on':onlyContent}">
       <span class="icon-check_circle"></span>
       <span class="text">只看有内容的评价</span>
     </div>
@@ -41,6 +41,14 @@
                   };
               }
           }
+      },
+      methods: {
+        select(type, event) {
+          if (!event._constructed) {
+            return;
+          }
+          this.selectType = type;
+        }
       }
   };
 </script>
@@ -53,5 +61,44 @@
       padding :18px
       margin :0 18px
       border-1px(rgba(7,17,27,0.1))
-
+      font-size :0
+      .block
+        display :inline-block
+        padding :8px 12px
+        margin-right :8px
+        line-height :16px
+        border-radius :1px
+        font-size :12px
+        color :rgb(77,85,93)
+        &.active
+          color :#fff
+        .count
+          margin-left :2px
+          font-size :8px
+        &.positive
+          background :rgba(0,160,220,0.2)
+          &.active
+            background :rgb(0,160,220)
+        &.negative
+          background :rgba(77,85,93,0.2)
+          &.active
+            background :rgb(77,85,93)
+  .switch
+    padding :12px 18px
+    line-height :24px
+    border-bottom :1px solid rgba(7,17,27,0.1)
+    color :rgb(147,153,159)
+    &.on
+     .icon-check_circle
+        color :#00c850
+    font-size :0
+    .icon-check_circle
+      display :inline-block
+      vertical-align :top
+      margin-right :4px
+      font-size :24px
+    .text
+      display :inline-block
+      vertical-align :top
+      font-size :12px
 </style>
--- src/components/split/split.vue
+++ src/components/split/split.vue
@@ -12,5 +12,5 @@
     height :16px
     border-top :1px solid rgba(7,17,27,0.1)
     border-bottom :1px solid rgba(7,17,27,0.1)
-    background :f3f5f7
+    background :#f3f5f7
 </style>
